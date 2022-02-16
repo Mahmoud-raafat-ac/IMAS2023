4ff96d4c63904fdaee22d8d1b65e855c343a01b8
Mahmoud-raafat-ac <dodo2_20002002@yahoo.com>
2022-02-16 21:49:20 +0200

    new title

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ahmed',});
+  res.render('index', { title: 'mahmoud',});
 });
 
 router.post('/', function(req, res, next) {
