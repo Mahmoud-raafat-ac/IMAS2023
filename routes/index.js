c215ed7e4695e7177f249403f0d09e12d165e3b9
Mahmoud-raafat-ac <dodo2_20002002@yahoo.com>
2022-03-16 12:22:38 +0200

    event site

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('Home');
+});
+
+router.get('/About', function(req, res, next) {
+  res.render('About');
+});
+
+router.get('/Topics', function(req, res, next) {
+  res.render('Topics');
+});
+
+router.get('/Structure', function(req, res, next) {
+  res.render('Structure');
+});
+
+router.get('/Steering-Committee', function(req, res, next) {
+  res.render('Steering-Committee');
+});
+
+router.get('/Sponsers', function(req, res, next) {
+  res.render('Sponsers');
 });
 
 module.exports = router;
